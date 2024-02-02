f3edc0caafa06dea43ad94f6a1d9f9bae1a6f236
amillerklugman <126700713+amillerklugman@users.noreply.github.com>
2024-02-02 11:15:53 -0500

    uploading data

---
+++ Lab_animal_data/Correlation_Table.R
@@ -0,0 +1,41 @@
+# --------------------------------------------------
+#
+#   Andrew Miller-Klugman
+#   Correlational Table
+#   Started: 2023-02-01
+#
+# --------------------------------------------------
+
+# Load in Libraries
+library(readr)
+library(readxl)
+library(ggplot2)
+library(tidyr)
+
+# Load in the needed data
+#TbCl Data 
+viable_spores <-
+read_excel("lab_animal_data/data/TbCl_swabs_viable_spores.xlsx")
+
+total_spore <-
+  read_excel("lab_animal_data/data/tbcl_total_spores_swab.xlsx")
+
+frozen_spore_viable <-
+  read_excel("lab_animal_data/data/TbCl_assay_post_freeze.xlsx", sheet = 4)
+
+frozen_spore_total <-
+  read_excel("lab_animal_data/data/TbCl_assay_post_freeze.xlsx", sheet = 2)
+
+#cell stain data 
+cell_stains <-
+  read_excel("lab_animal_data/data/CFU_cell_counts.xlsx", sheet = 7)
+
+#cell culture data 
+cell_culture_fresh <-
+  read_excel("lab_animal_data/data/CFU_cell_counts.xlsx", sheet = 3)
+
+cell_culture_frozen <-
+  read_excel("lab_animal_data/data/CFU_cell_counts.xlsx", sheet =5)
+
+
+
