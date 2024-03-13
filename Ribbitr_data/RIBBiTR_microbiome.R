77c582a0c954515dfe81b1780b9569eabfad549e
Patrick Kearns <kearnspj23@gmail.com>
2024-03-12 20:52:24 -0400

    added percent dormancy calcs

--- Ribbitr_data/RIBBiTR_microbiome.R
+++ Ribbitr_data/RIBBiTR_microbiome.R
@@ -20,13 +20,13 @@ meta2 <-  read.delim("Ribbitr_data/data/2022_Panama_16s_map.txt", header =T)
 
 # function used to Merge all asv tables together 
 MyMerge <- function(x, y){
-  df <- merge(x, y, by= "X.OTU.ID", all.x= TRUE, all.y= TRUE)
+  df <- merge(x, y, by= "OTU.ID", all.x= TRUE, all.y= TRUE)
   return(df)
 }
 asv_table <- Reduce(MyMerge, list(asv_table1, asv_table2))
 
 # fix formatting of asv table
-asv_table <- column_to_rownames(asv_table, var ="X.OTU.ID")
+asv_table <- column_to_rownames(asv_table, var ="OTU.ID")
 asv_table <- replace(asv_table, is.na(asv_table), 0)
 
 # merge all metadata together
@@ -68,7 +68,7 @@ ko.coords_loc <- ko.coords |>
   filter(Location != "")
 
 #plot PCoA by location 
-ggplot(ko.coords_loc, aes(MDS1, MDS2, color=Location))+
+ggplot(ko.coords_loc, aes(MDS1, MDS2, color=Location, shape=Type))+
   geom_point(aes(size=2))+
   #geom_text()+
   theme_bw()+
@@ -84,7 +84,7 @@ ko.coords_species <- ko.coords |>
   filter(Species != "")
 
 #plot PCoA by species
-ggplot(ko.coords_species, aes(MDS1, MDS2, color=Species))+
+ggplot(ko.coords_species, aes(MDS1, MDS2, color=Species, shape=Type))+
   geom_point(aes(size=2))+
   #geom_text()+
   theme_bw()+
@@ -125,6 +125,7 @@ ggplot(larv_alph_spec, aes(Species, Richness, fill=Location))+
   theme_bw()+
   xlab("")+
   coord_flip()+
+  facet_wrap(~Type)+
   ylab("sOTU Richness")
 #scale_fill_manual(values = c('#f58231', '#4363d8'))
 
@@ -135,6 +136,62 @@ ggplot(larv_alph_spec, aes(Species, Shannon, fill=Location))+
   theme_bw()+
   xlab("")+
   coord_flip()+
+  facet_wrap(~Type)+
   ylab("Shannon Diversity")
 #scale_fill_manual(values = c('#f58231', '#4363d8'))
+
+#write alphpa diversity to file
+write.table(larv_alph_spec, 'ribbitr_alpha_div.txt', sep='\t', quote=F, row.names=F)
+
+####Calculate percent dormant based on 16S ratios, rarefied data
+nut_rare2<-as.data.frame(t(nut_rare))
+
+#create RNA/DNA only tables for calculations
+library(dplyr)
+pan_rna<-select(nut_rare2, contains(".RNA"))
+pan_dna<-select(nut_rare2, contains(".DNA"))
+
+#sort columns to be in the same order
+pan_dna<-pan_dna[,order(colnames(pan_dna))]
+pan_rna<-pan_rna[,order(colnames(pan_rna))]
+
+#figure out which samples don't have matches
+#dna_name<-as.data.frame(names(pan_dna))
+#rna_name<-as.data.frame(names(pan_rna))
+#write.table(dna_name, 'dna.txt', sep='\t', quote=F, row.names = F)
+#write.table(rna_name, 'rna.txt', sep='\t', quote=F, row.names = F)
+
+#prune non matching samples
+pan_dna<-select(pan_dna, -c(Pan22.DNA, Pan23.DNA, Pan41.DNA, Pan44.DNA, Pan25.DNA,PA97.DNA,	PA81.DNA,	PA43.DNA,	PA205.DNA,	PA16.DNA,	PA194.DNA,	PA147.DNA,	BR5.DNA,	BR41.DNA,	BR13.DNA,	BR14.DNA))
+pan_rna<-select(pan_rna, -c(Pan24.RNA, Pan8.RNA, Pan32.RNA, Pan40.RNA, PA44.RNA, Pan39.RNA, Pan8.RNA, PA204.RNA,	BR71.RNA,	BR38.RNA))
+
+#check size
+dim(pan_dna)
+dim(pan_rna)
+
+#divide tables to get 16S ratio
+pan_ratio<-(pan_rna+1)/(pan_dna+1)
+
+#calculate percent dormant
+above_1 <- colSums(pan_ratio >= 1)
+
+# Total number of rows
+total_rows <- nrow(pan_ratio)
+
+# Calculate the proportion of rows above or equal to 1 for each column
+results <- above_1 / total_rows
+
+# Create a new data frame to store the results
+pan_dorm <- data.frame(Column = names(results), Proportion = results)
+
+#append metadata for stuff
+pan_dorm<-merge(pan_dorm, meta, by.x='Column', by.y='SampleID')
+
+#plot by species/site
+ggplot(pan_dorm, aes(Species, Proportion, fill=region))+
+  theme_bw()+
+  xlab("")+
+  geom_boxplot()+
+  ylab("Percent active")+
+  coord_flip()
 
